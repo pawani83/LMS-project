e61cf83a768afff9c83b94445f6b852418622f5e
Musthak Ahamed <115539833+musthakk@users.noreply.github.com>
2023-08-02 17:22:57 +0530

    Update Admin.php

--- LMS project/Librarian/Admin.php
+++ LMS project/Librarian/Admin.php
@@ -10,8 +10,28 @@
         <!-- Bootstrap 5 icons library !-->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
-
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        
+    
+        <?php
+            if (isset($_GET['data']))
+            {
+                $Details = $_GET['data'];
+            }
+
+            session_start();
+            $_SESSION['details'] = $Details;
+
+            if (isset($_POST['Logout'])) {
+
+                // Redirect the user to the login page
+                header("Location: http://localhost/LMS%20project/Log-in%20and%20sign-up/login.php");
+                exit;
+            }
+        ?>
+
+
         <style>
             body{
                 background-color: lightyellow;
@@ -46,79 +66,90 @@
     </head>
 
     <body>
-        <div class="row px-4 pt-3 pb-3 bg-brown"> 
-            <div class="col-sm-9">
-                <p class="display-4" style="color:white"><b>LibraNET</b></p>
+        <div class="container-fluid">
+            <div class="row px-4 pt-3 pb-3 bg-brown"> 
+                <div class="col-sm-9">
+                    <p class="display-4" style="color:white"><b>LibraNET</b></p>
+                </div>
+                <div class="col-sm-3 d-flex justify-content-end">
+                    <span style="font-size:60px; color: white;"><?php echo $Details[0]['Admin_Id']; ?>_</span>
+                    <a href="#" data-bs-toggle="collapse" data-bs-target="#demo"><i class="bi bi-person-circle" style="font-size:60px; color:white" ></i></a> 
+                </div>
+                <div id="demo" class="collapse text-end">
+                    <form method="post">
+                        <button type="submit" name="Logout" class="btn btn-danger">Logout</button>
+                    </form>
+                </div>  
             </div>
-            <div class="col-sm-3 d-flex justify-content-end">
-                <span style="font-size:60px; color: white;">A1_</span>
-                <a href="#"><i class="bi bi-person-circle" style="font-size:60px; color:white" ></i></a>   
-             </div>
         </div>
         
 
-        
-        <div class="row mx-5 mt-5">
-            <div class="col-sm-1"></div>
-            <div class="col-sm-4 pt-4 pb-4 px-2 bg-brown text-lightyellow text-center rounded-4">
-                <p class="display-6">BOOKS</p>
-                <a href="Manage%20Books/ManageBooks.php" class="btn btn-lg btn-success">MANAGE</a>
-            </div>
-            <div class="col-sm-2"></div>
-            <div class="col-sm-4 pt-4 pb-4 px-2 bg-brown text-lightyellow text-center rounded-4">
-                <p class="display-6">MEMBERS</p>
-                <a href="#" class="btn btn-lg btn-success">MANAGE</a>
-            </div>
-            <div class="col-sm-1"></div>
-        </div><br>
-        
-        <div class="row mx-5 mt-5">
-            <div class="col-sm-1"></div>
-            <div class="col-sm-4 pt-4 pb-4 px-2 bg-brown text-lightyellow text-center rounded-4">
-                <p class="display-6">PERSONAL INFO</p>
-                <a href="#" class="btn btn-lg btn-success">MANAGE</a>
-            </div>
-            <div class="col-sm-2"></div>
-            <div class="col-sm-4 pt-4 pb-4 px-2 bg-brown text-lightyellow text-center rounded-4">
-                <p class="display-6">ADMINS</p>
-                <a href="#" class="btn btn-lg btn-success">MANAGE</a>
-            </div>
-            <div class="col-sm-1"></div>
-        </div><br>
+        <div class="container-fluid d-flex flex-column min-vh-100" style="padding-right: 0; padding-left: 0;">    
+            <div class="row mx-5 mt-5">
+                <div class="col-sm-1"></div>
+                <div class="col-sm-4 pt-4 pb-4 px-2 bg-brown text-lightyellow text-center rounded-4">
+                    <p class="display-6">BOOKS</p>
+                    <a href="Manage%20Books/ManageBooks.php" class="btn btn-lg btn-success">MANAGE</a>
+                </div>
+                <div class="col-sm-2"></div>
+                <div class="col-sm-4 pt-4 pb-4 px-2 bg-brown text-lightyellow text-center rounded-4">
+                    <p class="display-6">MEMBERS</p>
+                    <a href="#" class="btn btn-lg btn-success">MANAGE</a>
+                </div>
+                <div class="col-sm-1"></div>
+            </div><br>
+            
+            <div class="row mx-5 mt-5">
+                <div class="col-sm-1"></div>
+                <div class="col-sm-4 pt-4 pb-4 px-2 bg-brown text-lightyellow text-center rounded-4">
+                    <p class="display-6">PERSONAL INFO</p>
+                    <a href="#" class="btn btn-lg btn-success">MANAGE</a>
+                </div>
+                <div class="col-sm-2"></div>
+                <div class="col-sm-4 pt-4 pb-4 px-2 bg-brown text-lightyellow text-center rounded-4">
+                    <p class="display-6">ADMINS</p>
+                    <a href="#" class="btn btn-lg btn-success">MANAGE</a>
+                </div>
+                <div class="col-sm-1"></div>
+            </div><br>
 
-        <div class="row mx-5 mt-5">
-            <div class="col-sm-1"></div>
-            <div class="col-sm-4 pt-4 pb-4 px-2 bg-brown text-lightyellow text-center rounded-4">
-                <p class="display-6">PAST BORROW DETAILS</p>
-                <a href="#" class="btn btn-lg btn-success">SHOW</a>
-            </div>
-            <div class="col-sm-2"></div>
-            <div class="col-sm-4 pt-4 pb-4 px-2 bg-brown text-lightyellow text-center rounded-4">
-                <p class="display-6">SELLERS</p>
-                <a href="#" class="btn btn-lg btn-success">MANAGE</a>
-            </div>
-            <div class="col-sm-1"></div>
-        </div><br>
+            <div class="row mx-5 mt-5">
+                <div class="col-sm-1"></div>
+                <div class="col-sm-4 pt-4 pb-4 px-2 bg-brown text-lightyellow text-center rounded-4">
+                    <p class="display-6">PAST BORROW DETAILS</p>
+                    <a href="#" class="btn btn-lg btn-success">SHOW</a>
+                </div>
+                <div class="col-sm-2"></div>
+                <div class="col-sm-4 pt-4 pb-4 px-2 bg-brown text-lightyellow text-center rounded-4">
+                    <p class="display-6">SELLERS</p>
+                    <a href="#" class="btn btn-lg btn-success">MANAGE</a>
+                </div>
+                <div class="col-sm-1"></div>
+            </div><br>
 
-        <div class="row mx-5 mt-5">
-            <div class="col-sm-1"></div>
-            <div class="col-sm-4 pt-4 pb-4 px-2 bg-brown text-lightyellow text-center rounded-4">
-                <p class="display-6">PUBLISHERS</p>
-                <a href="#" class="btn btn-lg btn-success">MANAGE</a>
-            </div>
-            <div class="col-sm-2"></div>
-            <div class="col-sm-4 pt-4 pb-4 px-2 bg-brown text-lightyellow text-center rounded-4">
-                <p class="display-6">CHANGE PASSWORD</p>
-                <a href="#" class="btn btn-lg btn-success">CHANGE</a>
-            </div>
-            <div class="col-sm-1"></div>
-        </div><br>
+            <div class="row mx-5 mt-5">
+                <div class="col-sm-1"></div>
+                <div class="col-sm-4 pt-4 pb-4 px-2 bg-brown text-lightyellow text-center rounded-4">
+                    <p class="display-6">PUBLISHERS</p>
+                    <a href="#" class="btn btn-lg btn-success">MANAGE</a>
+                </div>
+                <div class="col-sm-2"></div>
+                <div class="col-sm-4 pt-4 pb-4 px-2 bg-brown text-lightyellow text-center rounded-4">
+                    <p class="display-6">CHANGE PASSWORD</p>
+                    <a href="#" class="btn btn-lg btn-success">CHANGE</a>
+                </div>
+                <div class="col-sm-1"></div>
+            </div><br>
 
-        <!-- Footer Element of the page !-->
-        <footer class="container-fluid mt-5 pt-3 bg-dark text-center text-white">
-            <div class=" d-flex align-items-center justify-content-center">
-                <p>&copy; Copyright 2023 LibraNET. All rights reserved.</p>
-            </div>
-        </footer>
+            <!-- Spacer to push the footer to the bottom when content height is not enough -->
+            <div class="flex-grow-1"></div>
+
+            <!-- Footer Element of the page !-->
+            <footer class="container-fluid mt-5 pt-3 bg-dark text-center text-white w-100">
+                <div class=" d-flex align-items-center justify-content-center">
+                    <p>&copy; Copyright 2023 LibraNET. All rights reserved.</p>
+                </div>
+            </footer>
+        </div>
     </body>
 </html>
